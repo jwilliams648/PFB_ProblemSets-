0963dd0373f4af816d8c4ce7ccc529d8ac16f3a5
Jelani Williams <jelani@Jelanis-MBP.lan1>
2020-03-16 17:10:48 -0400

    adding

---
+++ problemsets/sys_example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print("My Name: "+ name +"\n","My fav color: "+color+"\n", "My fav activity: "+activity+"\n","My fav animal: "+animal)
+
+
